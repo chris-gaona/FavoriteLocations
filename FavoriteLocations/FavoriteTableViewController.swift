b675304ddae3de264dc1e36881ff5d781071c5be
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-22 07:53:23 -0700

    Implements UIAlertController when a cell is selected

--- FavoriteLocations/FavoriteTableViewController.swift
+++ FavoriteLocations/FavoriteTableViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class FavoriteTableViewController: UITableViewController {
+    /* Table Implementation */
     lazy var dataSource = configureDataSource()
     
     enum Section {
@@ -43,5 +44,29 @@ class FavoriteTableViewController: UITableViewController {
             return cell
         })
         return dataSource
+    }
+    /* End Table Implementation */
+    
+    /* Row did select handling */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: "Traveling!", message: "Would you like to see more?", preferredStyle: .actionSheet)
+        
+        // Add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        let moreAction = UIAlertAction(title: "Show more!", style: .default) { (action: UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Not available yet", message: "Sorry this feature is not available yet. Please try later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        optionMenu.addAction(moreAction)
+        
+        // Display the menu
+        self.present(optionMenu, animated: true, completion: nil)
+        
+        // Deselect the row
+        tableView.deselectRow(at: indexPath, animated: false)
     }
 }
