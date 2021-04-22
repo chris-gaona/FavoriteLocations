c47cbb2b29e7b593cf86bf545d79ec5333a1eb2a
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-21 20:55:26 -0700

    Adds memories to the table cells

--- FavoriteLocations/FavoriteTableViewController.swift
+++ FavoriteLocations/FavoriteTableViewController.swift
@@ -15,6 +15,7 @@ class FavoriteTableViewController: UITableViewController {
     }
     
     var locations = ["Rome", "Arizona", "Half Moon Bay", "South Africa", "Maui", "Yellowstone"]
+    var memories = ["Oh how the mighty fall in the gladiator ring!", "The heat of the son makes my skin bubble like hot oil", "For such an amazing beach, why can't you be more sunny?", "How I wish to go back & explore more!", "My home away from home in the tropical paradise", "Wolves, bears, & bison oh my!"]
     var images = ["rome", "arizona", "hmb", "south-africa", "maui", "yellowstone"]
 
     override func viewDidLoad() {
@@ -36,6 +37,7 @@ class FavoriteTableViewController: UITableViewController {
             cellProvider: {tableView, indexPath, location in let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! FavoriteTableViewCell
             
             cell.locationLabel.text = location
+            cell.memoryLabel.text = self.memories[indexPath.row]
             cell.thumbnailImageView.image = UIImage(named: self.images[indexPath.row])
                 
             return cell
