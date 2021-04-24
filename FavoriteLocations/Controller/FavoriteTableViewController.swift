05a5c363d8545406e4266b1f093f7a7a32b761df
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-24 08:06:49 -0700

    Uses a struct to manage the data

--- FavoriteLocations/Controller/FavoriteTableViewController.swift
+++ FavoriteLocations/Controller/FavoriteTableViewController.swift
@@ -15,31 +15,36 @@ class FavoriteTableViewController: UITableViewController {
         case all
     }
     
-    var locations = ["Rome", "Arizona", "Half Moon Bay", "South Africa", "Maui", "Yellowstone"]
-    var memories = ["Oh how the mighty fall in the gladiator ring!", "The heat of the son makes my skin bubble like hot oil", "For such an amazing beach, why can't you be more sunny?", "How I wish to go back & explore more!", "My home away from home in the tropical paradise", "Wolves, bears, & bison oh my!"]
-    var images = ["rome", "arizona", "hmb", "south-africa", "maui", "yellowstone"]
+    var locations: [Location] = [
+        Location(name: "Rome", memory: "Oh how the mighty fall in the gladiator ring!", image: "rome"),
+        Location(name: "Arizona", memory: "The heat of the son makes my skin bubble like hot oil", image: "arizona"),
+        Location(name: "Half Moon Bay", memory: "For such an amazing beach, why can't you be more sunny?", image: "hmb"),
+        Location(name: "South Africa", memory: "How I wish to go back & explore more!", image: "south-africa"),
+        Location(name: "Maui", memory: "My home away from home in the tropical paradise", image: "maui"),
+        Location(name: "Yellowstone", memory: "Wolves, bears, & bison oh my!", image: "yellowstone"),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.separatorStyle = .none
         
-        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Location>()
         snapshot.appendSections([.all])
         snapshot.appendItems(locations, toSection: .all)
         dataSource.apply(snapshot, animatingDifferences: false)
     }
     
-    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, Location> {
         let cellIdentifier = "tablecell"
         
-        let dataSource = UITableViewDiffableDataSource<Section, String> (
+        let dataSource = UITableViewDiffableDataSource<Section, Location> (
             tableView: tableView,
             cellProvider: {tableView, indexPath, location in let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! FavoriteTableViewCell
             
-            cell.locationLabel.text = location
-            cell.memoryLabel.text = self.memories[indexPath.row]
-            cell.thumbnailImageView.image = UIImage(named: self.images[indexPath.row])
+                cell.locationLabel.text = location.name
+                cell.memoryLabel.text = location.memory
+                cell.thumbnailImageView.image = UIImage(named: location.image)
                 
             return cell
         })
---
+++ FavoriteLocations/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  FavoriteLocations
+//
+//  Created by Chris Gaona on 4/24/21.
+//
+
+import Foundation
+
+struct Location: Hashable {
+    var name: String = ""
+    var memory: String = ""
+    var image: String = ""
+}
