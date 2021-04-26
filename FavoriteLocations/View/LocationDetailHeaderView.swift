dd8a7112b5c860b45338b36534ac28ffc1335e00
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-26 16:34:36 -0700

    Uses custom font on detail view

--- FavoriteLocations/View/LocationDetailHeaderView.swift
+++ FavoriteLocations/View/LocationDetailHeaderView.swift
@@ -13,6 +13,10 @@ class LocationDetailHeaderView: UIView {
     @IBOutlet var nameLabel: UILabel! {
         didSet {
             nameLabel.numberOfLines = 0
+            
+            if let customFont = UIFont(name: "Nunito-Bold", size: 40.0) {
+                nameLabel.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: customFont)
+            }
         }
     }
     @IBOutlet var heartButton: UIButton!
